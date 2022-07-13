007ace8bff51549515e7ad350dc33800a9f29d59
Christian Mugisha <christian.m@gtbank.local>
2022-07-13 18:44:42 +0200

    second crud commit

--- Web-React-BackEnd/server.js
+++ Web-React-BackEnd/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+    host:
+    user:
+    password:
+    database:
+})
+
 
 app.get('/',(req,res) => {
     res.send('Hello World for my Gs');
 });
+
+
+//
+const port = 3001;
 
 app.listen( port , () => {
     console.log(`our server is listening on port ${port}`);
